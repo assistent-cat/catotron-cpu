da0985c398cdd41da2b57801d08bd1e869d2de0f
Ciaran O'Reilly <ciaran@futureofvoice.com>
2021-01-24 22:27:34 +0100

    Implementats ordinals singulars

--- text/numbers_ca.py
+++ text/numbers_ca.py
@@ -9,24 +9,28 @@ with io.open(f"{filepath}/ca.sor", 'r', encoding="utf-8") as prg:
 
 _separador_milers_re = re.compile(r'([0-9][0-9\.]+[0-9]{3})')
 _decimal_re = re.compile(r'([0-9]+\,[0-9]+)')
-_ordinal_re = re.compile(r'[0-9]+(st|nd|rd|th)')
+_ordinal_ms_re = re.compile(r'([0-9]+)(r|er|n|on|t|rt|è|e|ne|nè)+(\b)')
+_ordinal_mp_re = re.compile(r'([0-9]+)(rs|ns|ts|ns)+(\b)')
+_ordinal_fs_re = re.compile(r'([0-9]+)(a|ra|na|ta)+(\b)')
+_ordinal_fp_re = re.compile(r'([0-9]+)(es)+(\b)')
 _cardinal_re = re.compile(r'[0-9]+')
 
 def _esborra_separador_milers(m):
   return m.group(1).replace('.', '')
 
-def _expand_ordinal(m):
-  return _inflect.number_to_words(m.group(0))
-
 def _num2text(m):
   return num2text.run(m.group(0))
 
-def _num2text_ordinal(m):
-  return num2text.run(f"ordinal {m.group(0)}")
+def _num2text_ordinal_m(m):
+  return num2text.run(f"ordinal {m.group(1)}") + m.group(3)
+
+def _num2text_ordinal_f(m):
+  return num2text.run(f"ordinal-feminine {m.group(1)}") + m.group(3)
 
 def normalize_numbers_ca(text):
   text = re.sub(_separador_milers_re, _esborra_separador_milers, text)
   text = re.sub(_decimal_re, _num2text, text)
-  text = re.sub(_ordinal_re, _num2text_ordinal, text)
+  text = re.sub(_ordinal_ms_re, _num2text_ordinal_m, text)
+  text = re.sub(_ordinal_fs_re, _num2text_ordinal_f, text)
   text = re.sub(_cardinal_re, _num2text, text)
   return text
--- text/numbers_ca_test.py
+++ text/numbers_ca_test.py
@@ -35,6 +35,56 @@ class NumbersCa(unittest.TestCase):
         self.assertEqual(normalize_numbers_ca("Va comprar pa, vi i llonganisses"), "Va comprar pa, vi i llonganisses")
         self.assertEqual(normalize_numbers_ca("El número guanyador és 1, 23, 55, 34"), "El número guanyador és u, vint-i-tres, cinquanta-cinc, trenta-quatre")
 
+    def test_ordinals_ms(self):
+        """
+        Converteix ordinals masculins singulars
+        """
+        self.assertEqual(normalize_numbers_ca("Va arribar 4t de 5"), "Va arribar quart de cinc")
+        self.assertEqual(normalize_numbers_ca("el 1r va ser ell"), "el primer va ser ell")
+        self.assertEqual(normalize_numbers_ca("el 3er, no va aguantar"), "el tercer, no va aguantar")
+        self.assertEqual(normalize_numbers_ca("2n"), "segon")
+        self.assertEqual(normalize_numbers_ca("2on"), "segon")
+        self.assertEqual(normalize_numbers_ca("4t"), "quart")
+        self.assertEqual(normalize_numbers_ca("4rt"), "quart")
+        self.assertEqual(normalize_numbers_ca("5è: remogueu la barreja"), "cinquè: remogueu la barreja")
+        self.assertEqual(normalize_numbers_ca("6e"), "sisè")
+        self.assertEqual(normalize_numbers_ca("6e"), "sisè")
+        self.assertEqual(normalize_numbers_ca("21nè"), "vint-i-unè")
+        self.assertEqual(normalize_numbers_ca("un 81ne de Palamós"), "un vuitanta-unè de Palamós")
 
+    def test_ordinals_fs(self):
+        """
+        Converteix ordinals femenins singulars
+        """
+        self.assertEqual(normalize_numbers_ca("1a"), "primera")
+        self.assertEqual(normalize_numbers_ca("3ra"), "tercera")
+        self.assertEqual(normalize_numbers_ca("2a"), "segona")
+        self.assertEqual(normalize_numbers_ca("2na"), "segona")
+        self.assertEqual(normalize_numbers_ca("4a."), "quarta.")
+        self.assertEqual(normalize_numbers_ca("pugi a la 4ta, després giri a l'esquerra"), "pugi a la quarta, després giri a l'esquerra")
+        self.assertEqual(normalize_numbers_ca("va quedar 5a en la classificació"), "va quedar cinquena en la classificació")
+        self.assertEqual(normalize_numbers_ca("la 5na vegada"), "la cinquena vegada")
+
+    @unittest.skip("Encara no ha estat implementat a soros")
+    def test_ordinals_mp(self):
+        """
+        Converteix ordinals masculins plurals
+        """
+        self.assertEqual(normalize_numbers_ca("1rs"), "primers")
+    
+    @unittest.skip("Encara no ha estat implementat a soros")
+    def test_ordinals_fp(self):
+        """
+        Converteix ordinals femenins plurals
+        """
+        self.assertEqual(normalize_numbers_ca("1es"), "primeres")
+    
+    @unittest.skip("Encara no ha estat implementat")
+    def test_fraccions(self):
+        """
+        Converteix fraccions
+        """
+        self.assertEqual(normalize_numbers_ca("1/2"), "mig")
+        
 if __name__ == '__main__':
     unittest.main()
