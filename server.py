856f9264a76dabb1806c0c4caab0493f5c3b3f0d
Ciaran O'Reilly <ciaran@futureofvoice.com>
2021-02-06 01:02:18 +0100

    Allows loading and synthesizing both ona and pau voices

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 import argparse
 import falcon
 import os
+import sys
 from synthesizer import Synthesizer
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 HTML_BODY = os.path.join(PROJECT_PATH, 'templates/demo.html')
+
+LOAD_VOICE = os.getenv('LOAD_VOICE') or 'ona'
+if LOAD_VOICE not in ["ona", "pau", "both"]:
+  print(f"LOAD_VOICE {LOAD_VOICE} not supported")
+  sys.exit(1)
+  
+AVAILABLE_VOICES = ["ona", "pau"] if LOAD_VOICE == 'both' else [LOAD_VOICE]
 
 class UIResource:
   def on_get(self, req, res):
@@ -14,19 +22,25 @@ class UIResource:
 
 class SynthesisResource:
   def on_get(self, req, res):
+    voice = req.params.get('voice') or AVAILABLE_VOICES[0]
+    if voice not in AVAILABLE_VOICES:
+      raise falcon.HTTPBadRequest()
     if not req.params.get('text'):
       raise falcon.HTTPBadRequest()
     if len(req.params.get('text')) > 150:
       raise falcon.HTTPBadRequest('String too long',
                                   'String length shorter than 150 is accepted.')
-    res.data = synthesizer.synthesize(req.params.get('text'))
+    res.data = synthesizers[voice].synthesize(req.params.get('text'))
     res.content_type = 'audio/wav'
 
-synthesizer = Synthesizer()
-# TODO load via config
-t_model_path = os.path.join(PROJECT_PATH, 'models/upc_ona_tacotron2.pt')
 v_model_path = os.path.join(PROJECT_PATH, 'models/melgan_onapau_catotron.pt')
-synthesizer.load(t_model_path, v_model_path)
+
+synthesizers = {}
+t_model_path = {}
+for voice in AVAILABLE_VOICES:
+  synthesizers[voice] = Synthesizer()
+  t_model_path = os.path.join(PROJECT_PATH, f"models/upc_{voice}_tacotron2.pt")
+  synthesizers[voice].load(t_model_path, v_model_path)
 
 app = falcon.API()
 app.add_route('/synthesize', SynthesisResource())
